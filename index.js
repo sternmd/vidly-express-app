370aaeb6147cbef042c07b6805dfef9829faf2ec
Maxwell Stern <stern.maxwell@gmail.com>
2018-11-07 11:02:17 -0600

    get request

--- index.js
+++ index.js
@@ -4,5 +4,18 @@ const app = express();
 
 app.use(express.json());
 
+// DUMMY DATA
+const genres = [
+  { id: 1, name: 'Drama'},
+  { id: 2, name: 'Thriller'},
+  { id: 3, name: 'Comedy'}
+];
+
+// GET
+app.get('/api/genres', (req, res) => {
+  res.send(genres)
+});
+
+// PORT
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
